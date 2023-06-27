4d83f6658289dda07374d1e735289f24c08d8383
TomaszSlupik <slupiktomasz@gmail.com>
2023-06-27 07:46:06 +0200

    change

--- src/components/Trips/Describetrips/Mpsusz2023/Mpsusz2023.jsx
+++ src/components/Trips/Describetrips/Mpsusz2023/Mpsusz2023.jsx
@@ -10,6 +10,9 @@ import Myscrollbox from '../../../../style/myscrollbox';
 import Leftheader from '../../../../style/mymuistyle/leftheader';
 import Centerheader from '../../../../style/mymuistyle/centerheader';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import Halfbox from "../../../../style/mymuistyle/halfbox";
+import Cardflex from "../../../../style/mymuistyle/cardflex";
+import TextDescribebox from "../../../../style/mymuistyle/textdescribebox";
 
 
 
@@ -25,13 +28,52 @@ export default function Mpsusz2023() {
             <BacktoPage />
             <div className="event">
             <ThemeProvider theme={theme}>
-                <Linetext>Grand Prix Polski Aquathlon</Linetext>
+                <Linetext>Mistrzostwa Polski w Suszu</Linetext>
                 <Line />
             </ThemeProvider>
             <Myscrollbox />
                 <div className="event__box">
-                    <Leftheader><DoubleArrowIcon />Dzień startu</Leftheader>
-                    <Centerheader>Bieg</Centerheader>
+                    <Leftheader><DoubleArrowIcon />Dzień przed startem</Leftheader>
+                    <Cardflex data-aos="fade-left">
+            <img
+              className="event__box-map"
+              src={process.env.PUBLIC_URL + "/img/img_2023/rumiaTwo.JPG"}
+              alt="pakiet startowy Rumia"
+            />
+            <Halfbox>
+              <TextDescribebox>
+                Po dwóch startach duathlonowych, rangi Mistrzostw Polski, gdzie zdobyłem dwa medale, przyszedł czas na start Triathlonowy. 
+                Wyjazd do Susza zaplanowany miałem na sobotę. Podróż minęła bardzo szybko, a nocleg miałem w Iławie.  
+              </TextDescribebox>
+            </Halfbox>
+          </Cardflex>
+
+          <Cardflex data-aos="fade-right">
+            <Halfbox>
+              <TextDescribebox>
+                Po przyjeździe, zakwaterowaliśmy się w apartamencie w Poznaniu.
+                Szybkie zakupy w żabce i mogliśmy podładować kcal przed
+                niedzielnym startem, które były zaplanowane na 13:00.
+              </TextDescribebox>
+            </Halfbox>
+            {window.innerWidth < 900 ? (
+              <img
+                style={{ marginLeft: "0em" }}
+                className="event__box-map"
+                src={process.env.PUBLIC_URL + "/img/img_2023/czepinThree.jpg"}
+                alt="Ładowanie kcal przed zawodami"
+              />
+            ) : (
+              <img
+                style={{ marginLeft: "1.2em" }}
+                className="event__box-map"
+                src={process.env.PUBLIC_URL + "/img/img_2023/czepinThree.jpg"}
+                alt="Ładowanie kcal przed zawodami"
+              />
+            )}
+          </Cardflex>
+
+        
                 </div>
             </div>
         </div>
