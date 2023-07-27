de033e7f6100c1b1e65fb784b10126649d054d5f
TomaszSlupik <slupiktomasz@gmail.com>
2023-07-27 11:39:53 +0200

    change

--- src/components/Trips/Describetrips/Bydgoszcz2023/Bydgoszcz2023.jsx
+++ src/components/Trips/Describetrips/Bydgoszcz2023/Bydgoszcz2023.jsx
@@ -57,7 +57,8 @@ export default function Bydgoszcz2023() {
                 W sobotę znajomi startowali na dłuższym dystansie 1/4, więc po
                 rozruchu 8km udaliśmy się, aby im kibicować. Z wyścigu byli
                 zadowoleni. Etap kolarski należał do Łukasza, natomiast biegowy
-                do Karola. Po starcie spędziliśmy czas w Expo, a wieczorkiem było wyjście na miasto. 
+                do Karola. Po starcie spędziliśmy czas w Expo, a wieczorkiem
+                było wyjście na miasto.
               </TextDescribebox>
             </Halfbox>
             {window.innerWidth < 900 ? (
@@ -81,18 +82,21 @@ export default function Bydgoszcz2023() {
             <DoubleArrowIcon />
             Dzień startu
           </Leftheader>
-          
 
           <div data-aos="fade-left">
-              <TextDescribebox>
-                  Pływanie odbywało się w rzece Brda. Z prądem rzeki udało się uzyskać tempo 01:03 min/100m i 0.475 KM przepłynąłem 
-                  w czasie 00:05:00. Szybka strefa zmian i wyjechałem na etap kolarski. Tydzień temu miałem wypadek na rowerze na zawodach 
-                  w Suszu, jeszcze nie doszedłem tego dnia do pełni zdrowia i etap kolarski pojechałem zachowawczo. 
-                  Bieg w moim wykonaniu był bardzo dobry, pobiegłem najlepszy czas na tych zawodach co pozwoliło uzyskać 3 miejsce w kat M30 oraz 4 miejsce OPEN. 
-              </TextDescribebox>
-            </div>
+            <TextDescribebox>
+              Pływanie odbywało się w rzece Brda. Z prądem rzeki udało się
+              uzyskać tempo 01:03 min/100m i 0.475 KM przepłynąłem w czasie
+              00:05:00. Szybka strefa zmian i wyjechałem na etap kolarski.
+              Tydzień temu miałem wypadek na rowerze na zawodach w Suszu,
+              jeszcze nie doszedłem tego dnia do pełni zdrowia i etap kolarski
+              pojechałem zachowawczo. Bieg w moim wykonaniu był bardzo dobry,
+              pobiegłem najlepszy czas na tych zawodach co pozwoliło uzyskać 3
+              miejsce w kat M30 oraz 4 miejsce OPEN.
+            </TextDescribebox>
+          </div>
 
-            <Cardflex data-aos="fade-right" style={{marginBottom: "1em"}}>
+          <Cardflex data-aos="fade-right" style={{ marginBottom: "1em" }}>
             <img
               className="event__box-map"
               src={process.env.PUBLIC_URL + "/img/img_2023/bydgoszczTwo.jpg"}
@@ -100,21 +104,20 @@ export default function Bydgoszcz2023() {
             />
             <Halfbox>
               <TextDescribebox>
-                Jak zawsze organizatorzy stanęli na wysokości zadania i za każde miejsce OPEN czy kategoriach wiekowych 
-                dostawaliśmy super nagrody i bardzo ładne statuetki. 
+                Jak zawsze organizatorzy stanęli na wysokości zadania i za każde
+                miejsce OPEN czy kategoriach wiekowych dostawaliśmy super
+                nagrody i bardzo ładne statuetki.
               </TextDescribebox>
             </Halfbox>
           </Cardflex>
 
-
-
-            <div data-aos="fade-left">
-              <img
-                className="championman__box-img"
-                src={process.env.PUBLIC_URL + "/img/img_2023/bydgoszczOne.jpg"}
-                alt="podium Enea Bydgoszcz Triathlon"
-              />
-            </div>
+          <div data-aos="fade-left">
+            <img
+              className="championman__box-img"
+              src={process.env.PUBLIC_URL + "/img/img_2023/bydgoszczOne.jpg"}
+              alt="podium Enea Bydgoszcz Triathlon"
+            />
+          </div>
         </div>
       </div>
     </div>
