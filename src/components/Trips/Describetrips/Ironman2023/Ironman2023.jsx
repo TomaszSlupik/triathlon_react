fdef657c0eb8a93409604bdb343d428fefc2fa34
TomaszSlupik <slupiktomasz@gmail.com>
2023-08-27 17:11:24 +0200

    change

--- src/components/Trips/Describetrips/Ironman2023/Ironman2023.jsx
+++ src/components/Trips/Describetrips/Ironman2023/Ironman2023.jsx
@@ -48,8 +48,8 @@ export default function Ironman2023() {
                 rowerowa i biegowa nie należy do najłatwiejszych z uwagi na duże
                 przewyższenia i podbiegi. Do Gdyni wyruszyliśmy dzień przed
                 startem i zameldowaliśmy się w Hotelu Mercure oddalonym 350 m od
-                plaży. Wybraliśmy pokój z widokiem na piękną plażę. Z pokoju widoki były 
-                przepiękne. 
+                plaży. Wybraliśmy pokój z widokiem na piękną plażę. Z pokoju
+                widoki były przepiękne.
               </TextDescribebox>
             </Halfbox>
           </Cardflex>
@@ -57,73 +57,78 @@ export default function Ironman2023() {
           <Cardflex data-aos="fade-right">
             <Halfbox>
               <TextDescribebox>
-                Po przyjeździe, udaliśmy się w stronę Expo, gdzie znajomi startowali na krótszym dystansie Sprint. 
-                My wymęczeni podróżą, oni startem, zdecydowaliśmy podładować kcal. Padło na pizzę, a następnie 
-                pyszne smakołyki z kawą. Trzeba było mieć nadmiar energii, ponieważ w niedzielę start był zaplanowany na 
-                godzinę 6:00. 
+                Po przyjeździe, udaliśmy się w stronę Expo, gdzie znajomi
+                startowali na krótszym dystansie Sprint. My wymęczeni podróżą,
+                oni startem, zdecydowaliśmy podładować kcal. Padło na pizzę, a
+                następnie pyszne smakołyki z kawą. Trzeba było mieć nadmiar
+                energii, ponieważ w niedzielę start był zaplanowany na godzinę
+                6:00.
               </TextDescribebox>
             </Halfbox>
             {window.innerWidth < 900 ? (
-                 <Swiper
-                 style={{ marginLeft: "0em", height: '100%' }}
-                 effect={"flip"}
-                 grabCursor={true}
-                 pagination={true}
-                 navigation={true}
-                 modules={[EffectFlip, Pagination, Navigation]}
-                 className="mySwiper event__box-map"
-                 color="primary"
-               >
-                 <div className="carousel__box-slide event__box-map"
-                 
-                 >
-                   <SwiperSlide
-                   
-                   >
-                     <img
-                       className="carousel__box-slide"
-                       src={process.env.PUBLIC_URL + "/img/img_2023/gdyniaFive.jpg"}
-                       alt="Ładowanie kcal przed zawodami"
-                     />
-                   </SwiperSlide>
-                   <SwiperSlide>
-                     <img
-                       className="carousel__box-slide"
-                       src={process.env.PUBLIC_URL + "/img/img_2023/gdyniaSix.jpg"}
-                       alt="Ładowanie kcal przed zawodami"
-                     />
-                   </SwiperSlide>
-                 </div>
-               </Swiper>
-            ) : (
-
               <Swiper
-            style={{ marginLeft: "1.2em" }}
-            effect={"flip"}
-            grabCursor={true}
-            pagination={true}
-            navigation={true}
-            modules={[EffectFlip, Pagination, Navigation]}
-            className="mySwiper carousel__box"
-            color="primary"
-          >
-            <div className="carousel__box-slide">
-              <SwiperSlide>
-                <img
-                  className="carousel__box-slide"
-                  src={process.env.PUBLIC_URL + "/img/img_2023/gdyniaFive.jpg"}
-                  alt="Ładowanie kcal przed zawodami"
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="carousel__box-slide"
-                  src={process.env.PUBLIC_URL + "/img/img_2023/gdyniaSix.jpg"}
-                  alt="Ładowanie kcal przed zawodami"
-                />
-              </SwiperSlide>
-            </div>
-          </Swiper>
+                style={{ marginLeft: "0em", height: "100%" }}
+                effect={"flip"}
+                grabCursor={true}
+                pagination={true}
+                navigation={true}
+                modules={[EffectFlip, Pagination, Navigation]}
+                className="mySwiper event__box-map"
+                color="primary"
+              >
+                <div className="carousel__box-slide event__box-map">
+                  <SwiperSlide>
+                    <img
+                      className="carousel__box-slide"
+                      src={
+                        process.env.PUBLIC_URL + "/img/img_2023/gdyniaFive.jpg"
+                      }
+                      alt="Ładowanie kcal przed zawodami"
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img
+                      className="carousel__box-slide"
+                      src={
+                        process.env.PUBLIC_URL + "/img/img_2023/gdyniaSix.jpg"
+                      }
+                      alt="Ładowanie kcal przed zawodami"
+                    />
+                  </SwiperSlide>
+                </div>
+              </Swiper>
+            ) : (
+              <Swiper
+                style={{ marginLeft: "1.2em" }}
+                effect={"flip"}
+                grabCursor={true}
+                pagination={true}
+                navigation={true}
+                modules={[EffectFlip, Pagination, Navigation]}
+                className="mySwiper carousel__box"
+                color="primary"
+              >
+                <div className="carousel__box-slide">
+                  <SwiperSlide>
+                    <img
+                      className="carousel__box-slide"
+                      src={
+                        process.env.PUBLIC_URL + "/img/img_2023/gdyniaFive.jpg"
+                      }
+                      alt="Ładowanie kcal przed zawodami"
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <img
+                      className="carousel__box-slide"
+                      src={
+                        process.env.PUBLIC_URL + "/img/img_2023/gdyniaSix.jpg"
+                      }
+                      alt="Ładowanie kcal przed zawodami"
+                    />
+                  </SwiperSlide>
+                </div>
+              </Swiper>
             )}
           </Cardflex>
 
@@ -134,12 +139,22 @@ export default function Ironman2023() {
 
           <div data-aos="fade-left">
             <TextDescribebox>
-            Enea IRONMAN Gdynia w wersji skróconej 180,2km🚴‍♂️ + 42,195 km 🏃‍♂️Zrobiłem to‼️‼️‼️I am an Ironman🤩1 Polak 🇵🇱 w kat, 6 open w kat M30, 13 OPEN.
-            Sam nie dowierzam, że to zrobiłem w takich warunkach, ale mając taki support @lolek_wu @olku123 , wszytko możemy i z tego miejsca chciałbym serdecznie dziękuję moim najbliższym! Również bardzo dziękuję @mareknegu który wspierał mnie na trasie.
-            Ze względu na duże fale, etap pływacki został odwołany i o 7:00 mieliśmy zacząć od roweru. Od 6:45, godzinę czasu czekałem bez bluzy aż ruszy moja strefa. Ruszając na etap kolarski, wyziębiony na 0,1km 😂😂wypad mi cały bidon z picie i tak 180km leciałem bez płynu, tylko zatrzymywałem się na punktach odżywczych. Po 20km już chciałem zejść z trasy!!!! Po 70km jechałem już w bluzie @endlesspain_team i zanim rozgrzałem organizm trochę minęło. Dostałem jeszcze dodatkowo 1’ kary za wyrzucenie żelu w nieodpowiednie miejsce.
-            Podsumowując wyniki z datasport:
-            🚴‍♂️240W - śr. 34km/h
-            🏃‍♂️ śr. 4’03 => 2h53😅
+              Enea IRONMAN Gdynia w wersji skróconej 180,2km🚴‍♂️ + 42,195 km
+              🏃‍♂️Zrobiłem to‼️‼️‼️I am an Ironman🤩1 Polak 🇵🇱 w kat, 6 open w kat
+              M30, 13 OPEN. Sam nie dowierzam, że to zrobiłem w takich
+              warunkach, ale mając taki support @lolek_wu @olku123 , wszytko
+              możemy i z tego miejsca chciałbym serdecznie dziękuję moim
+              najbliższym! Również bardzo dziękuję @mareknegu który wspierał
+              mnie na trasie. Ze względu na duże fale, etap pływacki został
+              odwołany i o 7:00 mieliśmy zacząć od roweru. Od 6:45, godzinę
+              czasu czekałem bez bluzy aż ruszy moja strefa. Ruszając na etap
+              kolarski, wyziębiony na 0,1km 😂😂wypad mi cały bidon z picie i
+              tak 180km leciałem bez płynu, tylko zatrzymywałem się na punktach
+              odżywczych. Po 20km już chciałem zejść z trasy!!!! Po 70km
+              jechałem już w bluzie @endlesspain_team i zanim rozgrzałem
+              organizm trochę minęło. Dostałem jeszcze dodatkowo 1’ kary za
+              wyrzucenie żelu w nieodpowiednie miejsce. Podsumowując wyniki z
+              datasport: 🚴‍♂️240W - śr. 34km/h 🏃‍♂️ śr. 4’03 => 2h53😅
             </TextDescribebox>
             <div className="event__box-img">
               <img
--- src/components/Trips/Describetrips/Poznan2023/Poznan2023.jsx
+++ src/components/Trips/Describetrips/Poznan2023/Poznan2023.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Poznan2023() {
-  return (
-    <div>Poznan2023</div>
-  )
+  return <div>Poznan2023</div>;
 }
