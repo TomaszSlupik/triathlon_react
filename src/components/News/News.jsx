d308ad1dcc40d3c410165c2251b67c806f044d02
TomaszSlupik <slupiktomasz@gmail.com>
2023-09-01 07:55:39 +0200

    change

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -20,50 +20,55 @@ export default function News() {
       <div className="news__header">Aktualności</div>
       <div className="news__box">
         <div className="news__box-wrapper">
-        <Box sx={{ flexGrow: 1 }}>
-          <ThemeProvider theme={theme}>
-          <Grid
-            container
-            spacing={{ xs: 2, sm: 2, md: 3, lg: 4 }}
-            columns={{ xs: 4, sm: 4, md: 12 }}
-          >
-            {message.map((el, index) => {
-              return (
-                <Grid item xs={6} sm={6} md={4}>
-                  <Link to={el.link}>
-                    <Card key={index}>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="240"
-                          image={el.img}
-                          alt="green iguana"
-                        />
-                        <CardContent>
-                          <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="div"
-                            className="news__box-title"
-                          >
-                            {el.name_news}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Data dodania: {el.data_add}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Link>
-                </Grid>
-              );
-            })}
-          </Grid>
-          </ThemeProvider>
-         
-        </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <ThemeProvider theme={theme}>
+              <Grid
+                container
+                spacing={{ xs: 2, sm: 2, md: 2, lg: 3, xl: 4 }}
+                columns={{ xs: 4, sm: 4, md: 4, lg: 12 }}
+              >
+                {message.map((el, index) => {
+                  return (
+                    <Grid item xs={6} sm={6} md={4}>
+                      <Link to={el.link}>
+                        <Card key={index}>
+                          <CardActionArea>
+                            <CardMedia
+                              component="img"
+                              height="240"
+                              image={el.img}
+                              alt="green iguana"
+                            />
+                            <div className="news__box-wrapper--content">
+                            <CardContent
+                            style={{minHeight: '100%'}}
+                            >
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                                className="news__box-title"
+                              >
+                                {el.name_news}
+                              </Typography>
+                              <Typography
+                                variant="body2"
+                                color="text.secondary"
+                              >
+                                Data dodania: {el.data_add}
+                              </Typography>
+                            </CardContent>
+                            </div>
+                          </CardActionArea>
+                        </Card>
+                      </Link>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </ThemeProvider>
+          </Box>
         </div>
-        
       </div>
     </div>
   );
