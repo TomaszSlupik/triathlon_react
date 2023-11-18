6fd2e4768b8ff2187cdea1b5821e68f7c312dc1d
TomaszSlupik <slupiktomasz@gmail.com>
2023-11-18 09:40:28 +0100

    change

--- src/components/Trips/Describetrips/Niepodleglosci2023/Niepodleglosci2023.jsx
+++ src/components/Trips/Describetrips/Niepodleglosci2023/Niepodleglosci2023.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import BacktoPage from "../BacktoPage";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../../../../style/mybreakpoints";
+import Linetext from "../../../../style/mymuistyle/linetext";
+import Line from "../../../../style/mymuistyle/line";
+import Myscrollbox from "../../../../style/myscrollbox";
+import Leftheader from "../../../../style/mymuistyle/leftheader";
+import Centerheader from "../../../../style/mymuistyle/centerheader";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 
 export default function Niepodleglosci2023() {
+  useEffect(() => {
+    Aos.init({ duration: 3000 });
+  }, []);
+
   return (
-    <div>Niepodleglosci2023</div>
-  )
+    <div>
+      <BacktoPage />
+      <div className="event">
+        <ThemeProvider theme={theme}>
+          <Linetext>33 Bieg Niepodległości</Linetext>
+          <Line />
+        </ThemeProvider>
+        <Myscrollbox />
+        <div className="event__box">
+          {/* <Leftheader><DoubleArrowIcon />Dzień startu</Leftheader>
+                <Centerheader>Bieg</Centerheader> */}
+          <div style={{ color: "#fff" }}>Relacja niebawem</div>
+        </div>
+      </div>
+    </div>
+  );
 }
