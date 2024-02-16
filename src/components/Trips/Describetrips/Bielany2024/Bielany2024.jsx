1ab2d5841febdd7f360f0b5a1b5e53856a78f314
TomaszSlupik <slupiktomasz@gmail.com>
2024-02-16 08:07:36 +0100

    change

--- src/components/Trips/Describetrips/Bielany2024/Bielany2024.jsx
+++ src/components/Trips/Describetrips/Bielany2024/Bielany2024.jsx
@@ -37,22 +37,53 @@ export default function Bielany2024() {
           <Cardflex data-aos="fade-left">
             <img
               className="event__box-map"
-              src={
-                process.env.PUBLIC_URL + "/img/img_2024/bielanyFive.JPG"
-              }
+              src={process.env.PUBLIC_URL + "/img/img_2024/bielanyFive.JPG"}
               alt="Start bieg o Puchar Bielan"
             />
             <Halfbox>
               <TextDescribebox>
-              Sezon 2024 rozpocząłem od Pucharu Bielan na dystansie 5 kilometrów, 
-              który odbył się na atestowanej trasie. Ten bieg słynie z silnej konkurencji, 
-              przyciągając do udziału najlepszych zawodników. Dla wielu biegaczy jest to 
-              ważne wydarzenie w kalendarzu sportowym, ponieważ stanowi doskonałą okazję do 
-              sprawdzenia swojej formy przed sezonem i rywalizacji z innymi zawodnikami 
-              na wymagającym dystansie 5 kilometrów. 
+                Sezon 2024 rozpocząłem od Pucharu Bielan na dystansie 5
+                kilometrów, który odbył się na atestowanej trasie. Ten bieg
+                słynie z silnej konkurencji, przyciągając do udziału najlepszych
+                zawodników. Dla wielu biegaczy jest to ważne wydarzenie w
+                kalendarzu sportowym, ponieważ stanowi doskonałą okazję do
+                sprawdzenia swojej formy przed sezonem i rywalizacji z innymi
+                zawodnikami na wymagającym dystansie 5 kilometrów.
               </TextDescribebox>
             </Halfbox>
           </Cardflex>
+          <div data-aos="fade-right">
+            <div className="maraton__box-img">
+              <img
+                className="maraton__box-img"
+                src={process.env.PUBLIC_URL + "/img/img_2024/bielanyThree.JPG"}
+                alt="Po biegu na 5km"
+              />
+              <div className="maraton__box-img--signature"></div>
+            </div>
+          </div>
+          <div data-aos="fade-left">
+            <TextDescribebox>
+              W udziale w biegu o Puchar Bielan, z mocną konkurencją, zdołałem
+              osiągnąć 5. miejsce w kategorii OPEN, 1. miejsce w swojej
+              kategorii wiekowej oraz byłem pierwszym triathlonistą na mecie.
+              Pomimo startu z pierwszym kilometrem w tempie 2’56”, a zakończenia
+              ostatniego kilometra w tempie 2’54”, niestety w środkowej części
+              trasy, tempo spadło do 3’10”/km i finalnie udało mi się osiągnąć
+              akceptowalny czas 15:22 na atestowanej trasie. Dzięki ubraniom
+              marki Rough Radical czułem się komfortowo i pewnie na trasie. Było
+              mi bardzo miło spotkać się z ekipą Run Radical i podzielić się
+              spostrzeżeniami po biegu. Kod rabatowy na ubrania -10%:
+              slupik.tomasz
+            </TextDescribebox>
+            <div className="event__box-img">
+              <img
+                className="event__box-img"
+                src={process.env.PUBLIC_URL + "/img/img_2024/bielanySix.JPG"}
+                alt="Zdjęcie z podium"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
