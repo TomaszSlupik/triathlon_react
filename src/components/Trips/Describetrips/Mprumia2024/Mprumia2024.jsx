af33fbc129a2b8f7580e04d2996a46375ca95474
TomaszSlupik <slupiktomasz@gmail.com>
2024-04-24 18:48:39 +0200

    change

--- src/components/Trips/Describetrips/Mprumia2024/Mprumia2024.jsx
+++ src/components/Trips/Describetrips/Mprumia2024/Mprumia2024.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import 'aos/dist/aos.css'
+import Aos from 'aos';
+import BacktoPage from '../BacktoPage';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../../../../style/mybreakpoints';
+import Linetext from '../../../../style/mymuistyle/linetext';
+import Line from '../../../../style/mymuistyle/line';
+import Myscrollbox from '../../../../style/myscrollbox';
+import Leftheader from '../../../../style/mymuistyle/leftheader';
+import Centerheader from '../../../../style/mymuistyle/centerheader';
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+
 
 export default function Mprumia2024() {
+
+    useEffect (()=> {
+        Aos.init({duration: 3000})
+      }, [])
+    
+
   return (
-    <div>Mprumia2024</div>
+    <div>
+        <BacktoPage />
+        <div className="event">
+        <ThemeProvider theme={theme}>
+            <Linetext>Mistrzostwa Polski Rumia</Linetext>
+            <Line />
+        </ThemeProvider>
+        <Myscrollbox />
+            <div className="event__box">
+                <Leftheader><DoubleArrowIcon />Dzień startu</Leftheader>
+                <Centerheader>Bieg</Centerheader>
+            </div>
+        </div>
+    </div>
   )
 }
+
+
+
+
